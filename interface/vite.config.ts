fe30963b45b6515f57ceb4381878325d165c1fc0
Lőrik Levente <levminer@levminer.com>
2023-05-27 17:04:08 +0200

    Update packages

--- interface/vite.config.ts
+++ interface/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import sveltePreprocess from "svelte-preprocess"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,12 @@ export default defineConfig({
 	server: {
 		port: 3000,
 	},
+	build: {
+		outDir: "../dist",
+	},
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./"),
+		},
+	},
 })
-
