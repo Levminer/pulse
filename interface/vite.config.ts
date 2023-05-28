6eea548d599abb0e12ceb7f24e05b711c7083bd4
Lőrik Levente <levminer@levminer.com>
2023-05-28 19:05:17 +0200

    tsconfig paths

--- interface/vite.config.ts
+++ interface/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import sveltePreprocess from "svelte-preprocess"
-import { resolve } from "path"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,16 +9,12 @@ export default defineConfig({
 		svelte({
 			preprocess: sveltePreprocess(),
 		}),
+		tsconfigPaths(),
 	],
 	server: {
 		port: 3000,
 	},
 	build: {
 		outDir: "../dist",
-	},
-	resolve: {
-		alias: {
-			"@": resolve(__dirname, "./"),
-		},
 	},
 })
